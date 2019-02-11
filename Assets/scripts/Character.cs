ca36fc602c3b7652772e563dcad1995fd52f38b0
Any <any2key@yandex.ru>
2019-02-11 18:07:59 +0300

    Пора заканчивать с прелюдиями

---
+++ Assets/scripts/Character.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character  {
+    public string Name;
+    public string NickName;
+    public string PrefabName;
+}
